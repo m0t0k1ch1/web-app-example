5a1ce921ccfd11454a20b274e00938d3ddb92ea4
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-08-24 05:15:43 +0900

    fix(backend): logging

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
-	"log"
 	"net/http"
+	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/pkg/errors"
+	"golang.org/x/exp/slog"
 
 	"github.com/m0t0k1ch1/web-app-sample/backend/config"
 )
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
 	conf := Config{
 		Server: config.ServerConfig{
 			Port: 8080,
@@ -26,13 +29,18 @@ func main() {
 
 	go func() {
 		if err := api.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatal(errors.Wrap(err, "failed to start server"))
+			fatal(errors.Wrap(err, "failed to start server"))
 		}
 	}()
 
 	<-ctx.Done()
 
 	if err := api.Shutdown(context.Background()); err != nil {
-		log.Fatal(errors.Wrap(err, "failed to shutdown server"))
+		fatal(errors.Wrap(err, "failed to shutdown server"))
 	}
+}
+
+func fatal(err error) {
+	slog.Error(err.Error())
+	os.Exit(1)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.21.0
 require (
 	connectrpc.com/connect v1.11.0
 	github.com/pkg/errors v0.9.1
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
 	golang.org/x/net v0.14.0
 	google.golang.org/protobuf v1.31.0
 )
