5d837f1bc942789d6726f0ad4e2e5e8390ef230c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-08-24 07:57:47 +0900

    feat(backend): cors

--- backend/cmd/backend/app.go
+++ backend/cmd/backend/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 
@@ -22,10 +23,35 @@ func NewApp(conf Config) *App {
 	mux := http.NewServeMux()
 	mux.Handle("/grpc/", http.StripPrefix("/grpc", grpcMux))
 
+	c := cors.New(cors.Options{
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+		},
+		AllowedHeaders: []string{
+			"Accept-Encoding",
+			"Content-Encoding",
+			"Content-Type",
+			"Connect-Protocol-Version",
+			"Connect-Timeout-Ms",
+			"Connect-Accept-Encoding",  // Unused in web browsers, but added for future-proofing
+			"Connect-Content-Encoding", // Unused in web browsers, but added for future-proofing
+			"Grpc-Timeout",             // Used for gRPC-web
+			"X-Grpc-Web",               // Used for gRPC-web
+			"X-User-Agent",             // Used for gRPC-web
+		},
+		ExposedHeaders: []string{
+			"Content-Encoding",         // Unused in web browsers, but added for future-proofing
+			"Connect-Content-Encoding", // Unused in web browsers, but added for future-proofing
+			"Grpc-Status",              // Required for gRPC-web
+			"Grpc-Message",             // Required for gRPC-web
+		},
+	})
+
 	return &App{
 		server: &http.Server{
 			Addr:    conf.Server.Addr(),
-			Handler: h2c.NewHandler(mux, &http2.Server{}),
+			Handler: c.Handler(h2c.NewHandler(mux, &http2.Server{})),
 		},
 	}
 }
