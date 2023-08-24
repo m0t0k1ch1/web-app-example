997fa73c759b1c4207c2db56e545f77902b588ae
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-08-24 17:15:06 +0900

    fix: project structure

--- backend/_sql/schema.sql
+++ backend/_sql/schema.sql
@@ -1,8 +1,1 @@
-CREATE TABLE tasks (
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  title VARCHAR(255) NOT NULL,
-  is_completed BOOLEAN DEFAULT false NOT NULL,
-  updated_at BIGINT UNSIGNED NOT NULL,
-  created_at BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin;
+../../schema/sql/schema.sql
