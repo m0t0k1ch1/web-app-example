acee42d1c41d56685505eef0e0d85fa1d5295a91
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-08-31 18:02:54 +0900

    fix(backend): error message

--- backend/cmd/app/app.go
+++ backend/cmd/app/app.go
@@ -27,7 +27,7 @@ func NewApp(ctx context.Context, conf Config) (*App, error) {
 	{
 		db, err := sql.Open("mysql", conf.MySQL.DSN())
 		if err != nil {
-			return nil, errors.Wrap(err, "failed to connect to MySQL")
+			return nil, errors.Wrapf(err, "failed to connect to db: %s", conf.MySQL.DBName)
 		}
 
 		env = &handler.Env{
