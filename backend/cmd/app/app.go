8ffe82fbd487e58c48d09ca3e80a9111ed618236
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-08-31 18:16:42 +0900

    refactor(backend): app initialization

--- backend/cmd/app/app.go
+++ backend/cmd/app/app.go
@@ -19,70 +19,77 @@ import (
 type App struct {
 	config Config
 
+	env    *handler.Env
 	server *http.Server
 }
 
 func NewApp(ctx context.Context, conf Config) (*App, error) {
-	var env *handler.Env
-	{
-		db, err := sql.Open("mysql", conf.MySQL.DSN())
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to connect to db: %s", conf.MySQL.DBName)
-		}
-
-		env = &handler.Env{
-			DB: db,
-		}
-	}
-
-	var srv *http.Server
-	{
-		grpcMux := http.NewServeMux()
-		grpcMux.Handle(appv1connect.NewTaskServiceHandler(
-			appv1.NewTaskServiceHandler(env),
-			connect.WithInterceptors(ValidationInterceptor),
-			connect.WithCodec(NewJSONCodec()),
-		))
-
-		mux := http.NewServeMux()
-		mux.Handle("/grpc/", http.StripPrefix("/grpc", grpcMux))
-
-		c := cors.New(cors.Options{
-			AllowedMethods: []string{
-				http.MethodGet,
-				http.MethodPost,
-			},
-			AllowedHeaders: []string{
-				"Accept-Encoding",
-				"Content-Encoding",
-				"Content-Type",
-				"Connect-Protocol-Version",
-				"Connect-Timeout-Ms",
-				"Connect-Accept-Encoding",  // Unused in web browsers, but added for future-proofing
-				"Connect-Content-Encoding", // Unused in web browsers, but added for future-proofing
-				"Grpc-Timeout",             // Used for gRPC-web
-				"X-Grpc-Web",               // Used for gRPC-web
-				"X-User-Agent",             // Used for gRPC-web
-			},
-			ExposedHeaders: []string{
-				"Content-Encoding",         // Unused in web browsers, but added for future-proofing
-				"Connect-Content-Encoding", // Unused in web browsers, but added for future-proofing
-				"Grpc-Status",              // Required for gRPC-web
-				"Grpc-Message",             // Required for gRPC-web
-			},
-		})
-
-		srv = &http.Server{
-			Addr:    conf.Server.Addr(),
-			Handler: c.Handler(h2c.NewHandler(mux, &http2.Server{})),
-		}
-	}
-
-	return &App{
+	app := &App{
 		config: conf,
+	}
 
-		server: srv,
-	}, nil
+	if err := app.initEnv(); err != nil {
+		return nil, errors.Wrap(err, "failed to initialize env")
+	}
+
+	app.initServer()
+
+	return app, nil
+}
+
+func (app *App) initEnv() error {
+	db, err := sql.Open("mysql", app.config.MySQL.DSN())
+	if err != nil {
+		return errors.Wrapf(err, "failed to connect to db: %s", app.config.MySQL.DBName)
+	}
+
+	app.env = &handler.Env{
+		DB: db,
+	}
+
+	return nil
+}
+
+func (app *App) initServer() {
+	grpcMux := http.NewServeMux()
+	grpcMux.Handle(appv1connect.NewTaskServiceHandler(
+		appv1.NewTaskServiceHandler(app.env),
+		connect.WithInterceptors(ValidationInterceptor),
+		connect.WithCodec(NewJSONCodec()),
+	))
+
+	mux := http.NewServeMux()
+	mux.Handle("/grpc/", http.StripPrefix("/grpc", grpcMux))
+
+	c := cors.New(cors.Options{
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+		},
+		AllowedHeaders: []string{
+			"Accept-Encoding",
+			"Content-Encoding",
+			"Content-Type",
+			"Connect-Protocol-Version",
+			"Connect-Timeout-Ms",
+			"Connect-Accept-Encoding",  // Unused in web browsers, but added for future-proofing
+			"Connect-Content-Encoding", // Unused in web browsers, but added for future-proofing
+			"Grpc-Timeout",             // Used for gRPC-web
+			"X-Grpc-Web",               // Used for gRPC-web
+			"X-User-Agent",             // Used for gRPC-web
+		},
+		ExposedHeaders: []string{
+			"Content-Encoding",         // Unused in web browsers, but added for future-proofing
+			"Connect-Content-Encoding", // Unused in web browsers, but added for future-proofing
+			"Grpc-Status",              // Required for gRPC-web
+			"Grpc-Message",             // Required for gRPC-web
+		},
+	})
+
+	app.server = &http.Server{
+		Addr:    app.config.Server.Addr(),
+		Handler: c.Handler(h2c.NewHandler(mux, &http2.Server{})),
+	}
 }
 
 func (app *App) Start() error {
