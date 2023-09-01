73e1c3959a627327344784b37e81c2ef1f11c349
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-09-01 22:48:00 +0900

    refactor(backend): config

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -8,22 +8,41 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+	kayac_config "github.com/kayac/go-config"
 	"github.com/pkg/errors"
 	"golang.org/x/exp/slog"
 
-	_ "github.com/go-sql-driver/mysql"
+	"backend/config"
 )
 
 var (
 	confPath = flag.String("config", "app.yaml", "path to config file")
 )
 
+func loadConfig(path string) (config.App, error) {
+	kayac_config.Delims("<%", "%>")
+
+	var conf config.App
+	if err := kayac_config.LoadWithEnv(&conf, path); err != nil {
+		return config.App{}, err
+	}
+
+	v := validator.New(validator.WithRequiredStructEnabled())
+	if err := v.Struct(conf); err != nil {
+		return config.App{}, errors.Wrap(err, "invalid config")
+	}
+
+	return conf, nil
+}
+
 func main() {
 	flag.Parse()
 
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 
-	conf, err := LoadConfig(*confPath)
+	conf, err := loadConfig(*confPath)
 	if err != nil {
 		fatal(errors.Wrap(err, "failed to load config"))
 	}
