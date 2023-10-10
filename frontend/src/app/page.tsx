a840d6b3c020c2e9f810376e5e915b370c20df51
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-10-11 07:56:47 +0900

    feat(frontend): init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">web-app-sample/frontend</h1>
+    </main>
+  );
+}
