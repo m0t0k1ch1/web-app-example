585c47d60daf905ebc0ef5e9d09014fd142160a8
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-11-16 01:19:59 +0900

    feat(frontend): ready to buidl

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">web-app-example/frontend</h1>
+      <h1 className="text-2xl font-semibold">web-app-example/frontend</h1>
     </main>
   );
 }
