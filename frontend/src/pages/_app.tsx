ce6121cd22b1e497e4b624881d9b842ede8867e2
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-02-12 17:26:46 +0900

    feat(frontend): reinit

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${inter.className} h-full`}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span>web-app-example/frontend</span>
+    </div>
+  );
+}
