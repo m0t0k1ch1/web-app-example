87011fb483035bb00af5a0eed20139957ea3191e
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-10 23:11:18 +0900

    feat(frontend): fetch tasks from backend

--- frontend/src/app/graphql.provider.ts
+++ frontend/src/app/graphql.provider.ts
@@ -1,14 +1,25 @@
 import { ApplicationConfig, inject } from '@angular/core';
 
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { relayStylePagination } from '@apollo/client/utilities';
 import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
+
   return {
     link: httpLink.create({ uri: 'http://localhost:8080/graphql' }),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      resultCaching: true,
+      typePolicies: {
+        Query: {
+          fields: {
+            tasks: relayStylePagination(),
+          },
+        },
+      },
+    }),
   };
 }
 
--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { MatRippleModule } from '@angular/material/core';
+
+import { Apollo, QueryRef, gql } from 'apollo-angular';
+
+const LIST_TASKS = gql`
+  query ListTasks($status: TaskStatus!, $after: String, $first: Int32!) {
+    tasks(status: $status, after: $after, first: $first) {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+`;
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MatRippleModule],
+  imports: [CommonModule, MatRippleModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  private router = inject(Router);
+  private apollo = inject(Apollo);
+
+  private listTasksQuery: QueryRef<any, any>;
+
+  public tasks: any[] = [];
+
+  constructor() {
+    this.listTasksQuery = this.apollo.watchQuery({
+      query: LIST_TASKS,
+      variables: {
+        status: 'UNCOMPLETED',
+        first: 1,
+      },
+    });
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.initTasks();
+  }
+
+  private async initTasks() {
+    let result = await this.listTasksQuery.result();
+
+    this.tasks.push(...result.data.tasks.edges.map((edge: any) => edge.node));
+
+    while (result.data.tasks.pageInfo.hasNextPage) {
+      result = await this.listTasksQuery.fetchMore({
+        variables: {
+          after: result.data.tasks.pageInfo.endCursor,
+        },
+      });
+
+      this.tasks.push(...result.data.tasks.edges.map((edge: any) => edge.node));
+    }
+  }
+
+  public onClickAddTaskButton(): void {
+    this.router.navigate(['hogepi']);
+  }
+}
