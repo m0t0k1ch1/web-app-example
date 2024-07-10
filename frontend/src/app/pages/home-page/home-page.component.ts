db44a7a77e4287798a584e45db20b7692a8bfd95
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-10 21:07:17 +0900

    feat(frontend): mark up home page

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [MatRippleModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
