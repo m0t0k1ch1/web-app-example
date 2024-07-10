b95ecf04ebbbecd5e86ca5fe47ce5c0d866cdb12
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-10 18:00:25 +0900

    feat(frontend): set up routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    title: 'Home',
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+    title: '404 Not Found',
+  },
+];
