008618691971d645927aa15bcb979fec6725d4ca
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-11 19:23:09 +0900

    refactor(frontend): use the color palette of primeng

--- frontend/src/app/pages/not-found-page/not-found-page.component.ts
+++ frontend/src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.css',
 })
