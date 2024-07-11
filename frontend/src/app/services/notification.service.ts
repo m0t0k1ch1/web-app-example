e904dc1bf9b777e06bea526ee603dccf98aa12d7
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-11 16:17:51 +0900

    feat(frontend): allow to show error toast

--- frontend/src/app/services/notification.service.ts
+++ frontend/src/app/services/notification.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import * as utils from '../utils';
 
@@ -6,10 +6,59 @@ import * as utils from '../utils';
   providedIn: 'root',
 })
 export class NotificationService {
+  private readonly TOAST_OVERLAY_ELEMENT_ID = 'toast-overlay';
+
   constructor() {}
 
   public notifyUnexpectedError(err: any): void {
-    // TODO: improve
-    console.log(`ERROR: ${utils.stringifyError(err)}`);
+    const toastOverlay = document.getElementById(this.TOAST_OVERLAY_ELEMENT_ID);
+    if (toastOverlay === null) {
+      console.error(err);
+      return;
+    }
+
+    const toast = document.createElement('div');
+    {
+      toast.className =
+        'flex flex-col gap-2 rounded-xl bg-red-500 p-4 text-white';
+
+      const upper = document.createElement('div');
+      {
+        upper.className = 'flex flex-row justify-between';
+
+        const title = document.createElement('p');
+        {
+          title.className = 'font-bold';
+          title.innerText = 'ERROR';
+        }
+
+        const closeButton = document.createElement('button');
+        {
+          closeButton.className = 'w-6 cursor-pointer';
+          closeButton.innerText = '×';
+          closeButton.onclick = () => {
+            toast.remove();
+          };
+        }
+
+        upper.appendChild(title);
+        upper.appendChild(closeButton);
+      }
+
+      const lower = document.createElement('div');
+      {
+        const description = document.createElement('p');
+        {
+          description.innerText = utils.stringifyError(err);
+        }
+
+        lower.appendChild(description);
+      }
+
+      toast.appendChild(upper);
+      toast.appendChild(lower);
+    }
+
+    toastOverlay.appendChild(toast);
   }
 }
