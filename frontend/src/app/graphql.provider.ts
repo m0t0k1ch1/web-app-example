bf752770e6c8f7d1e51309e0512ef99eb7945fc9
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-07-21 12:51:46 +0900

    refactor(frontend): update cache config

--- frontend/src/app/graphql.provider.ts
+++ frontend/src/app/graphql.provider.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, inject } from '@angular/core';
 
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-import { relayStylePagination } from '@apollo/client/utilities';
 import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 
@@ -12,16 +11,7 @@ export function apolloOptionsFactory(): ApolloClientOptions<any> {
 
   return {
     link: httpLink.create({ uri: environment.backend.url }),
-    cache: new InMemoryCache({
-      resultCaching: true,
-      typePolicies: {
-        Query: {
-          fields: {
-            tasks: relayStylePagination(),
-          },
-        },
-      },
-    }),
+    cache: new InMemoryCache(),
   };
 }
 
