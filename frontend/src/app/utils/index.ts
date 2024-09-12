9afd9ef3fb1dcd551e110339598b30d71478b12a
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2024-09-12 22:08:22 +0900

    refactor(frontend): refactor

--- frontend/src/app/utils/index.ts
+++ frontend/src/app/utils/index.ts
@@ -6,7 +6,7 @@ export function stringifyError(err: any): string {
   if (typeof err === 'string') {
     return err;
   }
-  if (err instanceof Error) {
+  if ('message' in err) {
     return err.message;
   }
 
